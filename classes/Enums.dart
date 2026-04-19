7a3abcbb9f1b2b51b6e3bbd6539d9393a2c0de9f
Amir Bgane <amirbgane200401@gmail.com>
2026-04-19 20:54:54 +0300

    удаление старых файлов classes после перемещения в lib

--- classes/Enums.dart
+++
@@ -1,5 +0,0 @@
-enum CoffeeType {
-  espresso,
-  cappuccino,
-  latte
-}
--- classes/ICoffee.dart
+++
@@ -1,8 +0,0 @@
-// Абстрактный класс-интерфейс для всех видов кофе
-abstract class ICoffee {
-  int get coffeeNeeded;   // сколько кофе нужно
-  int get milkNeeded;     // сколько молока нужно
-  int get waterNeeded;    // сколько воды нужно
-  int get price;          // цена
-  String get name;        // название
-}
